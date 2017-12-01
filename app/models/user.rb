561c21359aa1d1b7e81ee0c830dec0a102fe9c4a
Magnun A V Furtado <magnunavf@gmail.com>
2017-11-30 18:45:05 -0800

    feature: Use devise in User model
    - Besides this, fix the test env adding the action mailer
    default url options (devise prerequisite).

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable,
+         :confirmable
   EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
 
   validates_presence_of :name, :email
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,7 @@
 FactoryBot.define do
   factory :user, :class => User do
-    name     Faker::LordOfTheRings.unique.character
-    email    Faker::Internet.unique.free_email
+    name        Faker::LordOfTheRings.unique.character
+    email       Faker::Internet.unique.free_email
+    password    Faker::Witcher.location + Faker::Number.number(3).to_s
   end
 end
